0f2cf7c327d24dbbee449e0e56cd0b06d3a2b98b
River Alaqidy <riveralaqidy@Rivers-Air.lan>
2025-02-22 18:24:01 -0600

    used Media Manager API to retrieve thumbnails

---
+++ api/index.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+require_once('./class-PBS-Media-Manager-API-Client.php');
+
+$env = require __DIR__ . '/.env.php';
+
+$client = new PBS_Media_Manager_API_Client(
+    $client_id = $env['CLIENT_ID'], 
+    $client_secret = $env['CLIENT_SECRET'], 
+    $base_endpoint = $env['BASE_URL']);
+
+$pageNumber = rand(1, 20);
+
+try {
+
+    $shows = $client->get_shows(array('page-size' => 20, 'page' => $pageNumber));
+
+    echo json_encode($shows);
+
+} catch (Exception $e) {
+    echo json_encode(['error' => 'Error Retrieving Show: ' . $e->getMessage()]);
+}
+
+?>
